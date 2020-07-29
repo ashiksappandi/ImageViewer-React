f6e0e1d9d6eaebc75aa81240f3c65415df908029
narayaa <Sairam_234>
2020-07-29 20:25:17 +0530

    Login Logo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from './screens/login/Login'
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    <React.StrictMode>
+        <Login />
+    </React.StrictMode>,
     document.getElementById('root')
 );
---
+++ src/screens/login/Login.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import './Login.css';
+
+class Login extends Component{
+
+    render() {
+        return (
+            <div>
+                <div className='header'>
+                    <span className='hdr-logo'>Image Viewer</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
