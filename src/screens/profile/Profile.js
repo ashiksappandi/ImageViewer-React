de760d30f569db8982636ba1c650b2784fa3bd8c
narayaa <Sairam_234>
2020-08-10 23:46:32 +0530

    Profile Page Functionality - Header functionality

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -2,11 +2,16 @@ import React, {Component} from "react";
 import Header from "../../common/header/Header";
 
 class Profile extends Component {
+
+    logout = () => {
+        sessionStorage.clear();
+        this.props.history.replace('/');
+    }
+
     render() {
         return (
             <div>
                 <Header screen={"Profile"}
-                        searchHandler={this.onSearchEntered}
                         handleLogout={this.logout}/>
             </div>
         )
