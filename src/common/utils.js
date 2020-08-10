749d54a5661a0d73ec42c69297a5cd190d276304
narayaa <Sairam_234>
2020-08-10 23:11:35 +0530

    Home Page Functionality - Comment functionality

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,5 +1,5 @@
 export const constants = {
     userMediaUrl:"https://graph.instagram.com/me/media?fields=id,caption",
     userPostUrl:"https://graph.instagram.com",
-    access_token:"IGQVJYZAU9LeXJ4STJLNmt3amJTYXg1Si03alpVQzJxZAWVKSVpNekFvYm1haHNuQnlsMmZAzNWxma21POVFSMmZA3QlRXV2c3RHdOODdMbHFlSGJHUnNVNWJEQ2E2eldCaGhyaDl3ZA253VHBFOWRpSWdvMwZDZD"
+    access_token:"IGQVJXVFpxekl5WVFnYjBVNHRaOERRTzBDWkFvQXBpc3JtNVBpVWk3ZADRCRzZAqOHd5VnB0cWFVWFlzay05dllVRlMtWTZAHRW5TUEJadEFvSzItbmpuQjRSVXloeW1JM3FQbTUtSmg4dG5FaVhvZADZAUTgZDZD"
   }
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -14,8 +14,10 @@ import FavoriteIconFill from '@material-ui/icons/Favorite';
 import {constants} from "../../common/utils";
 import UserAvatar from "../../assets/logo.jpg";
 import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-class Home extends Component{
+class Home extends Component {
 
     constructor(props) {
         super(props);
@@ -28,65 +30,68 @@ class Home extends Component{
             postDetails: {},
             allPostDetails: [],
             filteredPostDetails: [],
+            comments: {},
+            currentComment: '',
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getAllPosts().then(r => this.getPostDetails());
     }
 
     getAllPosts = () => {
-        const headers = { 'Accept': 'application/json' }
+        const headers = {'Accept': 'application/json'}
         let that = this;
-        let url = constants.userMediaUrl+"&access_token="+sessionStorage.getItem('access-token');
-        return fetch(url,{
-            method:'GET',
+        let url = constants.userMediaUrl + "&access_token=" + sessionStorage.getItem('access-token');
+        return fetch(url, {
+            method: 'GET',
             headers
-        }).then((response) =>{
+        }).then((response) => {
             return response.json();
-        }).then((jsonResponse) =>{
+        }).then((jsonResponse) => {
             that.setState({
-                allPostsId:jsonResponse.data,
+                allPostsId: jsonResponse.data,
             });
         }).catch((error) => {
-            console.log('error user data',error);
+            console.log('error user data', error);
         });
     }
 
 
     getPostDetails = () => {
         this.state.allPostsId.forEach(post => {
-            const headers = { 'Accept': 'application/json' }
+            const headers = {'Accept': 'application/json'}
             let that = this;
-            let url = constants.userPostUrl+"/"+ post.id +"?fields=id,media_type,media_url,username,timestamp&access_token="+sessionStorage.getItem('access-token');
-            return fetch(url,{
-                method:'GET',
+            let url = constants.userPostUrl + "/" + post.id + "?fields=id,media_type,media_url,username,timestamp&access_token=" + sessionStorage.getItem('access-token');
+            return fetch(url, {
+                method: 'GET',
                 headers
-            }).then((response) =>{
+            }).then((response) => {
                 return response.json();
-            }).then((jsonResponse) =>{
+            }).then((jsonResponse) => {
                 jsonResponse.caption = post.caption;
                 jsonResponse.likes = Math.floor(Math.random() * 10);
                 jsonResponse.liked = false;
+//                jsonResponse.comments = [];
                 that.setState({
-                    postDetails:jsonResponse,
+                    postDetails: jsonResponse,
                 })
                 that.state.allPostDetails.push(that.state.postDetails);
                 that.state.filteredPostDetails.push(that.state.postDetails);
             }).catch((error) => {
-                console.log('error user data',error);
+                console.log('error user data', error);
             });
 
         })
     }
 
-    onSearchEntered = (value) =>{
+    onSearchEntered = (value) => {
         let newFilteredData = [];
         this.state.allPostDetails.filter(post => post.caption.toString().toLowerCase().includes(value)).map(newFilterPost => (
             newFilteredData.push(newFilterPost)
         ))
         this.setState({
-            filteredPostDetails:newFilteredData
+            filteredPostDetails: newFilteredData
         })
     }
 
@@ -95,7 +100,7 @@ class Home extends Component{
         this.props.history.replace('/');
     }
 
-    navigateToAccount = () =>{
+    navigateToAccount = () => {
         this.props.history.push('/profile');
     }
 
@@ -106,66 +111,108 @@ class Home extends Component{
 
         if (likedPost.liked) {
             this.setState({
-                isLiked:false,
+                isLiked: false,
             });
             likedPost.liked = false;
             likedPost.likes--;
-        }else {
+        } else {
             this.setState({
-                isLiked:true
+                isLiked: true
             });
             likedPost.liked = true;
             likedPost.likes++;
         }
     }
 
+    commentChangeHandler = (e) => {
+        this.setState({
+            currentComment: e.target.value
+        });
+    }
+
+    onAddCommentClicked = (id) => {
+        if (this.state.currentComment === "" || typeof this.state.currentComment === undefined) {
+            return;
+        }
+        let commentList = this.state.comments.hasOwnProperty(id) ?
+            this.state.comments[id].concat(this.state.currentComment) : [].concat(this.state.currentComment);
+
+        this.setState({
+            comments: {
+                ...this.state.comments,
+                [id]: commentList
+            },
+            currentComment: '',
+        })
+    }
+
 
     render() {
-        return(
+        return (
             <div>
                 <Header screen={"Home"}
-                    searchHandler={this.onSearchEntered}
-                    handleLogout={this.logout}
-                    handleAccount={this.navigateToAccount}/>
+                        searchHandler={this.onSearchEntered}
+                        handleLogout={this.logout}
+                        handleAccount={this.navigateToAccount}/>
 
                 <div className='card-main-content'>
 
                     {this.state.filteredPostDetails.map(item => (
                         <Card className="card-root" key={item.id}>
-                        <CardHeader
-                            avatar={
-                                <Avatar alt="Profile Pic" src={UserAvatar} className='avatar' style={{border: "1px solid #fff"}}/>
-                            }
-                            title={
-                                item.username
-                            }
-                            subheader={ new Date(item.timestamp).toLocaleString()}
-                        />
-                        <CardMedia
-                            className="card-media"
-                            image={item.media_url}
-                            title={item.caption}
-                        />
-                        <Divider variant="middle" className="divider-line"/>
-                        <CardContent>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                {item.caption.replace(/#[a-z]+/gi,'')}
-                            </Typography>
-                            <Typography variant="body2" className="hashtags" component="p">
-                                {
-                                    item.caption.toString().match(/#[a-z]+/gi) ?
-                                        item.caption.toString().match(/#[a-z]+/gi).toString().replace(/,+/g, ' ') : ''
+                            <CardHeader
+                                avatar={
+                                    <Avatar alt="Profile Pic" src={UserAvatar} className='avatar'
+                                            style={{border: "1px solid #fff"}}/>
                                 }
-                            </Typography>
-                        </CardContent>
-                        <CardActions disableSpacing>
-                            <IconButton aria-label="add to favorites" onClick={this.onLikeClicked.bind(this,item.id)}>
-                                { item.liked && <FavoriteIconFill style={{color:'#F44336'}}/>}
-                                {!item.liked && <FavoriteIconBorder/>}
-                            </IconButton>
-                            <span className="post-likes">{item.likes} likes</span>
-                        </CardActions>
-                    </Card>
+                                title={
+                                    item.username
+                                }
+                                subheader={new Date(item.timestamp).toLocaleString()}
+                            />
+                            <CardMedia
+                                className="card-media"
+                                image={item.media_url}
+                                title={item.caption}
+                            />
+                            <Divider variant="middle" className="divider-line"/>
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {item.caption.replace(/#[a-z]+/gi, '')}
+                                </Typography>
+                                <Typography variant="body2" className="hashtags" component="p">
+                                    {
+                                        item.caption.toString().match(/#[a-z]+/gi) ?
+                                            item.caption.toString().match(/#[a-z]+/gi).toString().replace(/,+/g, ' ') : ''
+                                    }
+                                </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites"
+                                            onClick={this.onLikeClicked.bind(this, item.id)}>
+                                    {item.liked && <FavoriteIconFill style={{color: '#F44336'}}/>}
+                                    {!item.liked && <FavoriteIconBorder/>}
+                                </IconButton>
+                                <span className="post-likes">{item.likes} likes</span>
+                            </CardActions>
+                            <div className="comment-section">
+                            {this.state.comments.hasOwnProperty(item.id) && this.state.comments[item.id].map((comment, index) => {
+                                return (
+                                    <div key={index}>
+                                        <Typography component="p">
+                                            <span style={{fontWeight: 'bold'}}>{item.username}</span>: {comment}
+                                        </Typography>
+                                    </div>
+                                )
+                            })}
+                                <TextField id={"comment-"+item.id} value={this.state.currentComment} label="Add a comment" className='comment-input'
+                                           onChange={this.commentChangeHandler}/>
+                                <Button onClick={this.onAddCommentClicked.bind(this, item.id)}
+                                        variant="contained" color="primary" className="comment-button">
+                                    ADD
+                                </Button>
+                            </div>
+
+                        </Card>
                     ))}
                 </div>
             </div>
